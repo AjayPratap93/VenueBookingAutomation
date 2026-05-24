95f4738838a809df516e547675c24c0b12faf034
Ajay Thakur <ajaythakur63@gmail.com>
2026-05-24 22:42:58 +0530

    Added Ajay and Login classes

---
+++ src/main/java/com/venue/Booking_management/Ajay.java
@@ -0,0 +1,15 @@
+package com.venue.Booking_management;
+
+/**
+ * Hello world!
+ *
+ */
+public class Ajay 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Ajay is now going for the automain ");
+    }
+}
---
+++ src/main/java/com/venue/Booking_management/Login.java
@@ -0,0 +1,16 @@
+package com.venue.Booking_management;
+
+/**
+ * Hello world!
+ *
+ */
+public class Login 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        System.out.println("Ajay is now going for the automain ");
+        System.out.println("Loging page going to launch ");
+    }
+}
